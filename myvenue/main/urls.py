ac55f32a62346472e59ddedb7698acc66a2d3ba7
Bso650 <kunwarbishow@gmail.com>
2021-03-02 15:18:41 +0545

    MV-14

--- myvenue/main/urls.py
+++ myvenue/main/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/profile/', views.profile, name='profile'),
+    path('search/', views.search, name='search'),
+    path('venue/<venue_name>', views.venue_detail, name="venue_detail")
+
 ]
--- myvenue/main/views.py
+++ myvenue/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Event, Venue, Order
+import datetime
 
 
 # Create your views here.
@@ -8,5 +10,38 @@ def home(request):
     return render(request, 'main/home.html', context)
 
 
+def is_available_on(venue, date):
+    results = Order.objects.filter(date=date)
+    venue_names = [result.name for result in results]
+    names = [venue.name for venue in venue_names]
+    if venue in names:
+        return False
+    return True
+
+
+def search(request):
+    date = request.GET.get('date')
+    city = request.GET.get('city')
+    results = Venue.objects.filter(city=city)
+    result_venues = [result.name for result in results]
+    date_object = datetime.datetime.strptime(date, "%d %b %Y").date()
+    final_results = []
+    for idx, venue in enumerate(result_venues):
+        if is_available_on(venue, date_object):
+            final_results.append(results[idx])
+
+    context = {
+        'results': final_results,
+        'date': date_object,
+        'city': city
+    }
+    return render(request, 'main/search_results.html', context)
+
+
 def profile(request):
     return HttpResponse("WELCOME TO PROFILE")
+
+
+def venue_detail(request, venue_name):
+    results = 
+    return render(request, 'main/venue_details.html', context)
